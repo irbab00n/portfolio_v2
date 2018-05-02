f60a1011abb82cdbdfe5e0d6a9153c143c55afba
Thomas Cosby <cosbyts@gmail.com>
2018-05-02 00:07:18 -0700

    Mobile friendly Navbar implemented
    - Currently working on creating the drop down menu for mobile

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -13,9 +13,17 @@ export default class App extends React.Component {
 
     return (
 
-      <div>
+      <div className="content-body">
 
         <Navigation />
+
+        <div className="navigation-gutter"/>
+
+        <div style={{height: '300vh', width: '98vw', margin: '0 1vw'}}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin orci at malesuada fringilla. Etiam tincidunt id tellus eu feugiat. Sed auctor tempor massa eget efficitur. Praesent eu metus malesuada, iaculis magna in, malesuada libero. Vivamus tincidunt venenatis tempus. Donec eu tincidunt velit.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin orci at malesuada fringilla. Etiam tincidunt id tellus eu feugiat. Sed auctor tempor massa eget efficitur. Praesent eu metus malesuada, iaculis magna in, malesuada libero. Vivamus tincidunt venenatis tempus. Donec eu tincidunt velit.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin orci at malesuada fringilla. Etiam tincidunt id tellus eu feugiat. Sed auctor tempor massa eget efficitur. Praesent eu metus malesuada, iaculis magna in, malesuada libero. Vivamus tincidunt venenatis tempus. Donec eu tincidunt velit.
+        </div>
 
       </div>
 
--- client/src/components/Navigation/index.jsx
+++ client/src/components/Navigation/index.jsx
@@ -3,15 +3,40 @@ import React from 'react';
 export default class Navigation extends React.Component {
   constructor(props) {
     super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    let wrapper = document.getElementsByClassName('mobile-nav-toggle');
+    wrapper[0].classList.toggle('is-active');
   }
 
   render() {
 
     return (
 
-      <div className="navigation">
-        Navigation
-      </div>
+      <section className="navigation">
+
+        <div className="navigation-logo-wrapper">
+          <a className="navigation-logo" href="/"></a>
+        </div>
+
+        <div className="navigation-nav-wrapper">
+          <nav>
+            <a className="navigation-nav-link" href="">Link 1</a>
+            <a className="navigation-nav-link" href="">Link 2</a>
+            <a className="navigation-nav-link" href="">Link 3</a>
+            <a className="navigation-nav-link" href="">Link 4</a>
+          </nav>
+        </div>
+
+        <div className="mobile-nav-wrapper">
+          <a className="mobile-nav-toggle" onClick={(e) => this.handleClick(e)}>
+            <span></span>
+          </a>
+        </div>
+
+      </section>
 
     );
 
