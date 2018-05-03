0996c8086ac234f1f604b19d33cf1d0007d66d6c
Thomas Cosby <cosbyts@gmail.com>
2018-05-03 16:29:47 -0700

    Fixed typo in filename

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -3,6 +3,7 @@ import Navigation from './components/Navigation/index.jsx';
 
 import './sass/main.scss';
 
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
