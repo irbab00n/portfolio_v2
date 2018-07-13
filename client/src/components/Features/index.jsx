a396c8f892ea746d6af855d3f40bbe292b4a7b0f
Thomas Cosby <cosbyts@gmail.com>
2018-07-13 08:09:53 -0700

    Moved scss file into appropriate folder for what it's purpose is
    - Experimented with creating reuasble sizes for components to fit together better like a grid
    
    - It works as well as intended, but it would interesting to discover if there is a way to stack a full height, half width container, then 2 half height, half width containers next to each other in a vertical stack.
    
    - Currently, the parent container has to maintain a flex: row in order for the items to wrap appropriately, but I would have to create a parent container that has the same dimension, just with a flex: column

--- client/src/components/Features/index.jsx
+++ client/src/components/Features/index.jsx
@@ -9,14 +9,17 @@ export default class Features extends React.Component {
     return (
       <section id="about">
         <div id="about-content">
-          <div className="container-test about-half-ch about-full-cw green-bg-test">
+          <div className="container-test about-full-ch about-half-cw green-bg-test">
             Half Height, Full width -- Box 1
           </div>
           <div className="container-test about-half-ch about-half-cw blue-bg-test">
             Half Height, Half width -- Box 2a
           </div>
-          <div className="container-test about-half-ch about-half-cw blue-bg-test">
+          <div className="container-test about-half-ch about-half-cw gray-bg-test">
             Half Height, Half width -- Box 2b
+          </div>
+          <div className="container-test about-half-ch about-half-cw blue-bg-test">
+            Half Height, Half width -- Box 2c
           </div>
           <div className="container-test about-full-ch about-full-cw gray-bg-test">
             Full Height, Full width -- Box 3
