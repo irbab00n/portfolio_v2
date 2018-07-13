f6205752611cef5d98f8d0095468d38601d5308c
Thomas Cosby <cosbyts@gmail.com>
2018-07-12 22:47:21 -0700

    began work on creating reusable content containers

--- client/src/components/Features/index.jsx
+++ client/src/components/Features/index.jsx
@@ -7,7 +7,22 @@ export default class Features extends React.Component {
 
   render() {
     return (
-      <section id="about" className="page" style={{zIndex: 1}}>ABOUT</section>
+      <section id="about">
+        <div id="about-content">
+          <div className="container-test about-half-ch about-full-cw green-bg-test">
+            Half Height, Full width -- Box 1
+          </div>
+          <div className="container-test about-half-ch about-half-cw blue-bg-test">
+            Half Height, Half width -- Box 2a
+          </div>
+          <div className="container-test about-half-ch about-half-cw blue-bg-test">
+            Half Height, Half width -- Box 2b
+          </div>
+          <div className="container-test about-full-ch about-full-cw gray-bg-test">
+            Full Height, Full width -- Box 3
+          </div>
+        </div>
+      </section>
     );
   }
 }
